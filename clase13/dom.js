73493a81ae2e907257a176e48dbd26a9c529da26
cesarv <cesarsla08@gmail.com>
2023-10-02 19:09:49 -0300

    ejercicios DOM

---
+++ clase13/dom.js
@@ -0,0 +1,20 @@
+// var parrafosHeader = header.getElementsByTagName("p");
+
+// var parrafos = document.getElementsByTagName("p");
+// console.log(parrafos[0]);
+
+// for (var i = 0; i < parrafos.length; i++) {
+//     console.log(parrafos[0].textContent);
+//   }
+
+// // Crear nodo de tipo Element
+// var parrafo = document.createElement("p");
+// // Crear nodo de tipo Text
+// var contenido = document.createTextNode("Hola Mundo!");
+// // Añadir el nodo Text como hijo del nodo Element
+// parrafo.appendChild(contenido);
+// // Añadir el nodo Element como hijo de la pagina
+// document.body.appendChild(parrafo);
+
+var parrafo = document.getElementById("provisional");
+// parrafo.parentNode.removeChild(parrafo);

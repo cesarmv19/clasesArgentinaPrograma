c8eb3c0be27ab632b5fe2844e58e8c9aa4216303
cesarv <cesarsla08@gmail.com>
2023-10-09 20:04:01 -0300

    Api rest ejemplo - p2

--- clase17/api.php
+++ clase17/api.php
@@ -1,28 +1,35 @@
 <?php
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $data = json_decode(file_get_contents('php://input'), true);
-    print_r($data);
-
-
-    $host = 'localhost';
+header('Content-Type: application/json');
+// echo $_SERVER['REQUEST_URI'];
+$host = 'localhost';
     $username = 'root';
     $password = '';
     $db = 'micro_api';
-    $conn = mysqli_connect($host, $username, $password, $db);
-
+    $conn = new mysqli($host, $username, $password, $db);
     if(!$conn){
         header('HTTP/1.1 500 Internal Server Error');
         echo json_encode(array('message' => 'Erro de db'));
         exit();
     }
-    $query = "INSERT INTO personas(nombre, email) VALUES('" . $data['nombre'] . "', '" . $data['email'] . "')";
-    $result = mysqli_query($conn, $query);
 
-    if(!$result){
-        header('HTTP/1.1 500 Internal Server Error');
-        echo json_encode(array('message' => 'Erro de db'));
-        exit();
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $data = json_decode(file_get_contents('php://input'), true);
+    // print_r($data);
+
+    if(isset($_POST['nombre'])){
+        $nombre = $_POST['nombre'];
+        // echo $_POST['nombre'];
+    }
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+    }
+    $sql = "INSERT INTO personas(nombre, email) VALUES('$nombre', '$email')";
+    // $result = mysqli_query($conn, $query);
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(array('message' => 'Persona creada con éxito'));
+    } else {
+        echo json_encode(array('message' => 'Error al crear persona: ' . $conn->error));
     }
 
 
@@ -31,7 +38,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     exit();
 }
 
-header('Content-Type: application/json');
-echo json_encode(array('message' => 'Bienvenido a mi micro-API'));
+// header('Content-Type: application/json');
+// echo json_encode(array('message' => 'Bienvenido a mi micro-API'));
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT * FROM personas";  
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $personas = [];
+        while ($row = $result->fetch_assoc()) {
+            $personas[] = $row;
+        }
+        echo json_encode($personas);//convierte el array en formato json
+    } else {
+        echo json_encode(array());
+    }  
+}
 ?>
